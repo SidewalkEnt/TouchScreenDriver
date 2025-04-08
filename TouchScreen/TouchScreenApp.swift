66f94664b9810439d343d14a1d713b6030b8ae56
heyjooo <heejoo_byun@naver.com>
2025-04-08 15:49:14 +0900

    Initial Commit

---
+++ TouchScreen/TouchScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchScreenApp.swift
+//  TouchScreen
+//
+//  Created by 변희주 on 4/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
