2288ba1980872174e88634ce386a9cd95de0df01
heyjooo <heejoo_byun@naver.com>
2025-04-18 12:02:55 +0900

    Feat - remove legacy code

--- TouchScreenExtension/TouchScreenExtension.cpp
+++ TouchScreenExtension/TouchScreenExtension.cpp
@@ -7,68 +7,6 @@
 
 #include "TouchScreenExtension.h"
 
-//#include <os/log.h>
-//#include <HIDDriverKit/HIDDriverKit.h>
-//#include <NetworkingDriverKit/IOUserNetworkEthernet.h>
-//
-//struct TouchScreenExtension_IVars
-//{
-//    OSArray *elements;
-//    
-//    struct {
-//        OSArray *elements;
-//    } digitizer;
-//};
-// 
-//#define _elements   ivars->elements
-//#define _digitizer  ivars->digitizer
-// 
-//bool TouchScreenExtension::init()
-//{
-//    os_log(OS_LOG_DEFAULT, "TouchScreenExtension init");
-// 
-//    if (!super::init()) {
-//        return false;
-//    }
-//    
-//    ivars = IONewZero(TouchScreenExtension_IVars, 1);
-//    if (!ivars) {
-//        return false;
-//    }
-//    
-//exit:
-//    return true;
-//}
-// 
-//void TouchScreenExtension::free()
-//{
-//    if (ivars) {
-//        OSSafeReleaseNULL(_elements);
-//        OSSafeReleaseNULL(_digitizer.elements);
-//    }
-//    
-//    IOSafeDeleteNULL(ivars, TouchScreenExtension_IVars, 1);
-//    super::free();
-//}
-// 
-//kern_return_t
-//IMPL(TouchScreenExtension, Start)
-//{
-//    kern_return_t ret;
-//    
-//    ret = Start(provider, SUPERDISPATCH);
-//    if (ret != kIOReturnSuccess) {
-//        Stop(provider, SUPERDISPATCH);
-//        return ret;
-//    }
-// 
-//    os_log(OS_LOG_DEFAULT, "Hello World");
-//    
-//    RegisterService();
-//    
-//    return ret;
-//}
-
 #include <os/log.h>
 #include <DriverKit/IOUserServer.h>
 #include <DriverKit/IOLib.h>
